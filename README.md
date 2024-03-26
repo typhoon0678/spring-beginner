2181621e63ccc897e3793b42db81ab67caed3281
typhoon0678 <typhoon0678@gmail.com>
2024-03-26 19:08:19 +0900

    spring initializr

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spring Boot 3 & Spring Framework 6 마스터하기! [최신판]
+[강의 링크](https://www.udemy.com/course/spring-boot-and-spring-framework-korean/?couponCode=GENAISALE24)
---
+++ learn-spring-framework/src/test/java/com/typhoon0678/learnspringframework/LearnSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.typhoon0678.learnspringframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
