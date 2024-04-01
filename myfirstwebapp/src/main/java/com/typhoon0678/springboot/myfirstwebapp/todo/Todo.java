cb4816929b630234865a85b8751fb8ca9c549802
typhoon0678 <typhoon0678@gmail.com>
2024-04-01 21:53:02 +0900

    Spring MVC, Security

---
+++ myfirstwebapp/src/main/java/com/typhoon0678/springboot/myfirstwebapp/todo/Todo.java
@@ -0,0 +1,84 @@
+package com.typhoon0678.springboot.myfirstwebapp.todo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+@Entity
+public class Todo {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String username;
+    @Size(min = 10, message = "Enter at least 10 characters")
+    private String description;
+    private LocalDate targetDate;
+    private boolean done;
+
+    public Todo() {
+
+    }
+
+    public Todo(int id, String username, String description, LocalDate localDate, boolean done) {
+        this.id = id;
+        this.username = username;
+        this.description = description;
+        this.targetDate = localDate;
+        this.done = done;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getTargetDate() {
+        return targetDate;
+    }
+
+    public void setTargetDate(LocalDate localDate) {
+        this.targetDate = localDate;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", description='" + description + '\'' +
+                ", localDate=" + targetDate +
+                ", done=" + done +
+                '}';
+    }
+}
---
+++ myfirstwebapp/src/main/java/com/typhoon0678/springboot/myfirstwebapp/todo/TodoRepository.java
@@ -0,0 +1,10 @@
+package com.typhoon0678.springboot.myfirstwebapp.todo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TodoRepository extends JpaRepository<Todo, Integer> {
+
+    public List<Todo> findByUsername(String username);
+}
